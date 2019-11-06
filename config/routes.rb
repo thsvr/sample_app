a377910f20a1872a2c35a89a086007044b3cd887
thsvr <vr.ths.zd@gmail.com>
2019-11-06 14:03:15 -0300

    add 'helf' to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get  '/help',    to: 'static_pages#help'
+  get  '/help',    to: 'static_pages#help', as: 'helf'
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
