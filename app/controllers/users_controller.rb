254d5e8ec20697e9d28c0acbd780b57cff476668
thsvr <vr.ths.zd@gmail.com>
2019-11-21 18:24:32 -0300

    fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,21 +9,19 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params[:user])   
+    @user = User.new(user_params)   
       if @user.save
-      # Handle a successful save.
     else
       render 'new'
     end
+  end
 
 
    private
-    
+
     def user_params
-      params.require(:user).permit(:name, :email, :password,
+      user_params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
-
-  end
 
 end
