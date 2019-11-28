b4dfb14680d724dfe4852b69244254ba48cdeed8
thsvr <vr.ths.zd@gmail.com>
2019-11-28 17:53:10 -0300

    dding an authenticated? method to the User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,16 +8,16 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
-class << self
+
   # Returns the hash digest of the given string.
-  def digest(string)
+  def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
 
   # Returns a random token.
-  def new_token
+  def User.new_token
     SecureRandom.urlsafe_base64
   end
 
@@ -26,5 +26,9 @@ class << self
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
   end
-end
+
+  # Returns true if the given token matches the digest.
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
 end
