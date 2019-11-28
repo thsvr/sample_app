309dc31b929c9a4d52dc3232978adc6c2ae2340e
thsvr <vr.ths.zd@gmail.com>
2019-11-28 17:56:05 -0300

    adding a remember to the user

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,10 +1,18 @@
 module SessionsHelper
-    # Logs in the given user.
+
+  # Logs in the given user.
   def log_in(user)
     session[:user_id] = user.id
   end
 
-# Returns the current logged-in user (if any).
+  # Remembers a user in a persistent session.
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  # Returns the current logged-in user (if any).
   def current_user
     if session[:user_id]
       @current_user ||= User.find_by(id: session[:user_id])
@@ -21,5 +29,4 @@ module SessionsHelper
     session.delete(:user_id)
     @current_user = nil
   end
-
 end
