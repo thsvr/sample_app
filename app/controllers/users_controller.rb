e82a40571712e4c0d53bcc86aa032e9d7b0c6623
thsvr <vr.ths.zd@gmail.com>
2019-11-29 16:27:43 -0300

    Adding a action correct_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -52,4 +52,11 @@ class UsersController < ApplicationController
         redirect_to login_url
       end
     end
+
+# Confirms the correct user.
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless @user == current_user
+  end
+
 end
