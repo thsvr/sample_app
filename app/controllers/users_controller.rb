5abf6abf866a3bd17d3c80ee2ec065ee926eb8e1
thsvr <vr.ths.zd@gmail.com>
2019-12-02 18:53:31 -0300

    adding a generalized authenticated? method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,7 +21,6 @@ class UsersController < ApplicationController
       UserMailer.account_activation(@user).deliver_now
       flash[:info] = "Please check your email to activate your account."
       redirect_to root_url
-
     else
       render 'new'
     end
