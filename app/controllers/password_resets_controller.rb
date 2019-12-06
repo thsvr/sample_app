ef55ecca60971f910fa6ad5d9d25ae8554c5ca63
thsvr <vr.ths.zd@gmail.com>
2019-12-06 15:04:13 -0300

    adding the update action for password reset

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,6 +1,7 @@
 class PasswordResetsController < ApplicationController
   before_action :get_user,   only: [:edit, :update]
   before_action :valid_user, only: [:edit, :update]
+  before_action :check_expiration, only: [:edit, :update]
 
   def new
   end
@@ -18,11 +19,31 @@ class PasswordResetsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if params[:user][:password].empty?                  # Case (3)
+      @user.errors.add(:password, "can't be empty")
+      render 'edit'
+    elsif @user.update_attributes(user_params)          # Case (4)
+      log_in @user
+      flash[:success] = "Password has been reset."
+      redirect_to @user
+    else
+      render 'edit'                                     # Case (2)
+    end
+  end
+
   private
 
-  def get_user
-    @user = User.find_by(email: params[:email])
-  end
+    def user_params
+      params.require(:user).permit(:password, :password_confirmation)
+    end
+
+    def get_user
+      @user = User.find_by(email: params[:email])
+    end
 
   # Confirms a valid user.
   def valid_user
@@ -32,6 +53,12 @@ class PasswordResetsController < ApplicationController
     end
   end
 
-  def edit
+  # Checks expiration of reset token.
+  def check_expiration
+    if @user.password_reset_expired?
+      flash[:danger] = "Password reset has expired."
+      redirect_to new_password_reset_url
+    end
   end
+
 end
