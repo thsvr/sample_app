1298c46ebbde197ae4ee364dc8416da0ca4edba9
thsvr <vr.ths.zd@gmail.com>
2019-12-06 18:01:05 -0300

    removing the logged_in_user method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -54,21 +54,12 @@ class UsersController < ApplicationController
                                    :password_confirmation)
     end
 
-    # Before filters
+  # Before filters
 
-    # Confirms a logged-in user.
-    def logged_in_user
-      unless logged_in?
-        #store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
-    end
-
-# Confirms the correct user.
+  # Confirms the correct user.
   def correct_user
     @user = User.find(params[:id])
-    redirect_to(root_url) unless @user == current_user
+    redirect_to(root_url) unless current_user?(@user)
   end
 
   # Confirms an admin user.
