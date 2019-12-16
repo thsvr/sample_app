3edb1929bae98dc18af7fe6e8aa4d380d2b054a9
thsvr <vr.ths.zd@gmail.com>
2019-12-16 16:58:40 -0300

    Implementing the active relationships has_many association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  has_many :active_relationships, class_name:  "Relationship",
+  foreign_key: "follower_id",
+  dependent:   :destroy
+
   has_many :microposts, dependent: :destroy
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
